7752ac4356202cfc3ae0f6b824681d8584e84b64
MEJRI ISSAM <34856078+imejri@users.noreply.github.com>
2022-01-28 13:12:46 +0100

    Update variable.sh

--- jenkins/variable.sh
+++ jenkins/variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "the workspace $WORKSPACE"
 echo "the artifactory server address is $ARTIFACTORY_SERVER"
-echo "the file name is jenkins-ape-'$RUNNING_VERSION'"
+echo "the file name is jenkins-ape-$RUNNING_VERSION"
 
