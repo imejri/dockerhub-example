4bb89e15765d97d088b7dfddb7efd7c40dc6b3c2
MEJRI ISSAM <34856078+imejri@users.noreply.github.com>
2022-01-30 18:30:55 +0100

    Update variable.sh

--- jenkins/variable.sh
+++ jenkins/variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "the config file is "$file""
+echo "the config file is $file"
 echo "the workspace $WORKSPACE"
 echo "the artifactory server address is $ARTIFACTORY_SERVER"
 echo "the file name is jenkins-ape-$RUNNING_VERSION"
