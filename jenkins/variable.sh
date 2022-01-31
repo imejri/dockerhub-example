3e7f8eefff547b692b43e03fb6fc7ce7cb4281e1
MEJRI ISSAM <34856078+imejri@users.noreply.github.com>
2022-01-31 08:23:59 +0100

    add ssh login

--- jenkins/variable.sh
+++ jenkins/variable.sh
@@ -3,4 +3,5 @@ echo "the config file is $file"
 echo "the workspace $WORKSPACE"
 echo "the artifactory server address is $ARTIFACTORY_SERVER"
 echo "the file name is jenkins-ape-$RUNNING_VERSION"
+echo "my ssh key is $MYKEY and my username is $USERNAME"
 
